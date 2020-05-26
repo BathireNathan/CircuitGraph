1f911c9f15901559d73ac622360f848d1934a7bb
bathirenathan <bathirenathan@nmsworks.co.in>
2020-05-26 20:59:04 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+class CircuitGraph {
+  constructor (newOptions) {
+    this.options = {};
+    console.log(this.options);
+    $.extend (this.options, newOptions);
+
+    console.log(this.options);
+  }
+
+
+}
